cf4a6c0a23bcf4fc478be7c7f70c56a64c51705f
Nicolas Burat <nburat-d@student.42.fr>
2022-02-15 15:12:56 +0100

    ft_is_int corrige a la norme

--- ft_is_int.c
+++ ft_is_int.c
@@ -6,11 +6,24 @@
 /*   By: nburat-d <nburat-d@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/02/12 18:58:42 by nburat-d          #+#    #+#             */
-/*   Updated: 2022/02/12 19:15:16 by nburat-d         ###   ########.fr       */
+/*   Updated: 2022/02/15 15:11:49 by nburat-d         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "./libft.h"
+
+void	del_zero(char *str, int i)
+{
+	int	len;
+
+	len = ft_strlen(str);
+	while (str[i] == '0' && str[i + 1] != '\0')
+	{
+		ft_memcpy(&str[i], &str[i + 1], ft_strlen(&str[i + 1]));
+		str[len - 1] = 0;
+		len--;
+	}	
+}
 
 int	ft_is_int(char *str)
 {
@@ -19,12 +32,10 @@ int	ft_is_int(char *str)
 	i = 0;
 	if (str[i] == '-' || str[i] == '+')
 		i++;
+	del_zero(str, i);
 	while (str[i])
-	{
-		if (ft_isdigit(str[i]) == 0)
+		if (ft_isdigit(str[i++]) == 0)
 			return (0);
-		i++;
-	}
 	if (ft_strlen(str) < 10)
 		return (1);
 	if (ft_strlen (str) > 11)
