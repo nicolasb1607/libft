3f6465fbef3c92be7a9c9d8d71bc3dfeff27a95b
Nicolas BURAT DE GURGY <nburat-d@e2r4p6.clusters.42paris.fr>
2022-06-16 16:37:47 +0200

    FIX ft_strcmp

--- srcs/ft_strcmp.c
+++ srcs/ft_strcmp.c
@@ -6,7 +6,7 @@
 /*   By: nburat-d <nburat-d@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/02/10 11:02:26 by nburat-d          #+#    #+#             */
-/*   Updated: 2022/02/10 11:06:27 by nburat-d         ###   ########.fr       */
+/*   Updated: 2022/06/16 16:36:57 by nburat-d         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,6 +17,8 @@ int	ft_strcmp(char *s1, char *s2)
 	int	i;
 
 	i = 0;
+	if(ft_strlen(s1) != ft_strlen(s2))
+		return (-1);
 	while (s1[i] && s2[i])
 	{
 		if (s1[i] != s2[i])
